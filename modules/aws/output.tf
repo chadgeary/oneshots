bf002d9545f16947d7c5908c46ad42d591c1d849
Chad Geary <c.geary@wearemetronome.com>
2024-08-18 00:26:47 -0400

    fmt

--- modules/aws/output.tf
+++ modules/aws/output.tf
@@ -4,8 +4,8 @@ output "this" {
     availability_zones = data.aws_availability_zones.this
     caller_identity    = data.aws_caller_identity.this
     default_tags       = data.aws_default_tags.this
-    partition       = data.aws_partition.this
-    region          = data.aws_region.this
-    session_context = data.aws_iam_session_context.this
+    partition          = data.aws_partition.this
+    region             = data.aws_region.this
+    session_context    = data.aws_iam_session_context.this
   }
 }
