8eac7864621b2c58c7f44278a0f954a8b8573681
Chad Geary <c.geary@wearemetronome.com>
2024-08-18 23:54:37 -0400

    lifecycle

--- modules/k3s/main-s3.tf
+++ modules/k3s/main-s3.tf
@@ -15,11 +15,27 @@ resource "aws_s3_bucket_policy" "this" {
   policy = data.aws_iam_policy_document.this-s3.json
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
   block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    id     = "90day-snapshots"
+    status = "Enabled"
+    filter {
+      prefix = "controlplane/snapshots/"
+    }
+    expiration {
+      days = 90
+    }
+  }
+  depends_on = [aws_s3_bucket_versioning.this]
 }
