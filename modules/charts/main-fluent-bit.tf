0ad7124ba814c1a1a4daae1bf135c9e94dcfbb6c
Chad <chad.geary@gmail.com>
2024-12-03 13:16:18 -0500

    Update Helm release fluent-bit to v0.48.3 (#111)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- modules/charts/main-fluent-bit.tf
+++ modules/charts/main-fluent-bit.tf
@@ -14,7 +14,7 @@ resource "helm_release" "this-fluent-bit" {
   name       = "fluent-bit"
   namespace  = "fluent-bit"
   repository = "https://fluent.github.io/helm-charts"
-  version    = "0.48.2"
+  version    = "0.48.3"
   values = [yamlencode({
     config = {
       inputs  = <<EOF
