8711791b138f8941fafbc0a84559588861c8b598
Chad <chad.geary@gmail.com>
2024-12-17 13:19:43 -0500

    Update Helm release kube-prometheus-stack to v66.7.1 (#122)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- modules/charts/main-prometheus.tf
+++ modules/charts/main-prometheus.tf
@@ -14,7 +14,7 @@ resource "helm_release" "this-prometheus" {
   name       = "prometheus"
   namespace  = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
-  version    = "66.3.1"
+  version    = "66.7.1"
   values = [yamlencode({
     alertmanager = {
       enabled = false
