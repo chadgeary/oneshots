48f564e828db5ab4ce2ce189cd8ae25994d47628
Chad <chad.geary@gmail.com>
2024-12-24 13:17:21 -0500

    Update Helm release ztunnel to v1.24.2 (#131)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- modules/charts/main-istio.tf
+++ modules/charts/main-istio.tf
@@ -83,7 +83,7 @@ resource "helm_release" "this-istio-ztunnel" {
   name       = "istio-ztunnel"
   namespace  = "istio-system"
   repository = "https://istio-release.storage.googleapis.com/charts"
-  version    = "1.24.1"
+  version    = "1.24.2"
   values = [yamlencode({
     resources = {
       limits = {
